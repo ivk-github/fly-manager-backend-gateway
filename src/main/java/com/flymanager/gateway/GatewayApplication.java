ed68b6eddb0015b2414b36284afad058afc3a57e
Ivan Katjounis <ivk.msg+github@gmail.com>
2021-07-14 11:14:11 +0200

    Refactor

--- src/main/java/com/flymanager/gateway/GatewayApplication.java
+++ src/main/java/com/flymanager/gateway/GatewayApplication.java
@@ -2,12 +2,10 @@ package com.flymanager.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
