3003c967e67981b5d9b26c35c4ed5176b151e04d
Ivan Katjounis <ivk.msg+github@gmail.com>
2021-07-17 20:56:59 +0200

    Add security

---
+++ src/main/java/com/flymanager/gateway/configuration/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.flymanager.gateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		http.csrf().disable();
+		http.authorizeExchange()
+			.anyExchange().authenticated();
+		http.oauth2ResourceServer().jwt();
+		return http.build();
+	}
+}
